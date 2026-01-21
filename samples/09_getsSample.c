d6f13d6fa5727ff747c5bc8eece9d91302d4b0cc
JUMTBOX <yjey12@naver.com>
2026-01-21 21:59:43 +0900

    ...

---
+++ samples/09_getsSample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+    char szName[32] = {0}; 
+    printf("이름을 입력하세요: ");
+    gets(szName);
+
+    printf("당신의 이름은 "); // 개행을 하지 않는다
+    puts(szName); // 반면 puts는 개행된다 여기서 szName parameter 타입은 char* (char 포인터)
+    puts("입니다.");
+}
